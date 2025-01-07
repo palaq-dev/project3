e24bd25c1e98910b3b147b7c4e2eaac9a34823fc
palaq-dev <jovanovic.dusan044@gmail.com>
2025-01-07 20:45:51 +0100

    Ispravka

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
-// jquery efekti
+let products = [];
 
 window.addEventListener('load', function () {
+    // jQuery efekti
     document.querySelector('.loading-screen').style.display = "none";
     $('h2, h5, p').hide().delay(500).fadeIn(2000).delay(5000);
     $('a, button').hover(function () {
@@ -21,61 +22,66 @@ window.addEventListener('load', function () {
         opacity: '1'
     }, 1000);
 
-    if (window.location.pathname.endsWith("/order-items.html")) {
-        fetchData("products", populateCart)
+    if (window.location.pathname == "/order-items.html" || window.location.pathname == "/order-items") {
+        fetchData("products", populateCart);
     }
 
-    if (window.location.pathname.endsWith("/index.html") || window.location.pathname.endsWith("/")) {
+    if (window.location.pathname == "/index.html" || window.location.pathname == "/") {
+        let showSuccessfullOrderModal = localStorage.getItem('showSuccessfullOrderModal');
+        if (showSuccessfullOrderModal) {
+            $('#orderSuccessfullyCreatedModal').modal('show');
+            localStorage.clear();
+        }
+
         fetchData("slider", populateSlider);
+        fetchData("testimonials", populateReviews);
     }
 
-    if (window.location.pathname.endsWith("/index.html") || window.location.pathname.endsWith("/")) {
-        fetchData("testimonials", populateReviews)
+    if (window.location.pathname == "/shop.html" || window.location.pathname == "/shop") {
+        fetchData("products", populateShop);
     }
 
-    if (window.location.pathname.endsWith("shop.html")) {
-        fetchData("products", popuniShop)
-    }
-
-    fetchData("social-media-footer", populateSocialMedia)
-    fetchData("navigation-footer", popuniNavigationFooter)
+    fetchData("social-media-footer", populateSocialMedia);
+    fetchData("navigation-footer", populateNavigationFooter);
 
     // Filtriranje i sortiranje
     $("#category-filter").change(() => {
-        fetchData("products", popuniShop)
+        fetchData("products", populateShop);
     })
     $("#rating-filter").change(() => {
-        fetchData("products", popuniShop)
+        fetchData("products", populateShop);
     })
     $("#sort-by").change(() => {
-        fetchData("products", popuniShop)
+        fetchData("products", populateShop);
     })
 
-    refreshProductNumber()
+    refreshProductNumber();
 });
 
-// ajax funkcija kojom dohvatam podatke
+// Ova funkcija koristi AJAX zahtev kako bi dobavila podatke iz JSON fajla na serveru
 function fetchData(fileName, callback) {
     $.ajax({
         url: "assets/json/" + fileName + ".json",
         method: "get",
+        datatype: "json",
         success: function (data) {
-            callback(data)
-        }
+            callback(data);
+        },
+        error: function (xhr) { console.log(xhr); }
     })
 }
 
-// pravljenje slajdera 
+// Ova funkcija generiše HTML kod za pravljenje slajdera sa proizvodima
 function populateSlider(products) {
     let sliderDiv = document.getElementById("sliderDiv");
     sliderDiv.innerHTML = products.map(product =>
         `<div class="carousel-item ${products[0] == product ? "active" : " "}">
             <img src="${product.src}" class="d-block w-100" alt="${product.alt}" />
-        </div> `).join(" ")
+        </div> `).join(" ");
 
 }
 
-// pravljenje testimonials-a
+// Funkcija koja generiše HTML kod za prikazivanje testimonijala
 function populateReviews(testimonials) {
     let testimonialsDiv = document.getElementById("testimonialsDiv")
     testimonialsDiv.innerHTML = testimonials.map(testimonial => `<div class="col-md-4">
@@ -89,25 +95,27 @@ function populateReviews(testimonials) {
             <p>${testimonial.text}</p>
         </div>
     </div>
-</div>`).join(" ")
+</div>`).join(" ");
 
 }
 
-// pravljenje drustvenih mreza
+// Funkcija koja generiše HTML kod za prikazivanje ikona društvenih mreža
 function populateSocialMedia(socialMedias) {
-    let socialMediaUl = document.getElementById("socialMediaUl")
-    socialMediaUl.innerHTML = socialMedias.map(socialMedia => `<li><a id="${socialMedia.id}" href="${socialMedia.href}" role="button"><i
-                                                                class="${socialMedia.icon}"></i>&nbsp;${socialMedia.text}</a></li>`).join(" ")
+    let socialMediaUl = document.getElementById("socialMediaUl");
+    socialMediaUl.innerHTML = socialMedias.map(socialMedia => `<li><a class="${socialMedia.class}" id="${socialMedia.id}" href="${socialMedia.href}" role="button"><i
+                                                                class="${socialMedia.icon}"></i>&nbsp;${socialMedia.text}</a></li>`).join(" ");
 }
 
-// pravljenje navigacionog menia
-function popuniNavigationFooter(navigationFooterItems) {
-    let navigationMenuUl = document.getElementById("navigationMenuUl")
+// Funkcija koja generiše HTML kod za prikazivanje stavki navigacionog menija u podnožju stranice
+function populateNavigationFooter(navigationFooterItems) {
+    let navigationMenuUl = document.getElementById("navigationMenuUl");
     navigationMenuUl.innerHTML = navigationFooterItems.map(navigationFooterItem => `<li><a href="${navigationFooterItem.url}"><i class="${navigationFooterItem.icon}"></i>&nbsp;${navigationFooterItem.text}</a></li>`).join(" ")
 }
 
-// pravljenje liste proizvoda
-function popuniShop(shopItems) {
+// Funkcija koja generiše HTML kod za prikazivanje liste proizvoda u prodavnici
+function populateShop(shopItems) {
+    products = shopItems;
+
     let shopItemsDiv = document.getElementById("shopItemsDiv")
     let category = document.getElementById("category-filter").value
     let rating = document.getElementById("rating-filter").value
@@ -142,30 +150,41 @@ function popuniShop(shopItems) {
             </div>
         </div>
     </div>
-</div>` ).join(" ")
+</div>` ).join(" ");
 }
 
-// dodavanje u cart
+// Funkcija koja dodaje proizvod u korpu
 function addToCart(idProizvoda) {
-    let cart = JSON.parse(localStorage.getItem("cart"))
-    if (!cart) {
-        cart = [idProizvoda]
-    } else {
-        cart.push(idProizvoda)
+    let product = products.find(x => x.id == idProizvoda);
+
+    if (product) {
+        $('#productAddedToCartName').text(product.title);
+        $('#addedToCartModal').modal('show');
+
     }
-    localStorage.setItem("cart", JSON.stringify(cart))
+
+    let cart = JSON.parse(localStorage.getItem("cart"));
+
+    if (!cart) {
+        cart = [idProizvoda];
+    } else {
+        cart.push(idProizvoda);
+    }
+    localStorage.setItem("cart", JSON.stringify(cart));
     refreshProductNumber()
 }
+
+// Funkcija koja ažurira broj proizvoda prikazanih u korpi
 function refreshProductNumber() {
-    let cart = JSON.parse(localStorage.getItem("cart"))
-    let brojProizvoda = cart ? cart.length : 0
-    document.getElementById("brojProizvodaSpan").innerHTML = brojProizvoda
+    let cart = JSON.parse(localStorage.getItem("cart"));
+    let productsNumber = cart ? cart.length : 0;
+    document.getElementById("productsNumberSpan").innerHTML = productsNumber;
 }
 
-// popunjavanje carta
+// Funkcija koja generiše HTML kod za prikazivanje proizvoda u korpi
 function populateCart(shopItems) {
-    let shopItemsDiv = document.getElementById("shopItemsDiv")
-    let cart = JSON.parse(localStorage.getItem("cart"))
+    let shopItemsDiv = document.getElementById("shopItemsDiv");
+    let cart = JSON.parse(localStorage.getItem("cart"));
     cart = cart ? cart : []
     let filteredShopItems = shopItems.filter(shopItem => cart.includes(shopItem.id));
     let totalPrice = 0
@@ -195,24 +214,26 @@ function populateCart(shopItems) {
             </div>
         </div>
     </div>
-</div>` ).join(" ")
+</div>` ).join(" ");
 }
 
+// Funkcija koja uklanja sve proizvode iz korpe
 function removeCart() {
     localStorage.removeItem("cart");
-    fetchData("products", populateCart)
+    fetchData("products", populateCart);
 }
 
+// Funkcija koja validira informacije unete u formi za naručivanje i prikazuje odgovarajući modal
 function orderNow() {
     const validationRules = [
         {
             input: document.getElementById("name").value,
-            regex: /^[A-ZŠĐĆČ]+(([',. -][a-zšđćčA-ZŠĐĆČ])?[a-zšđćčA-ZŠĐĆČ]*)*$/,
+            regex: /^[A-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/,
             errorMessage: "The name is mandatory and must have the first initial capital letter.",
         },
         {
             input: document.getElementById("last-name").value,
-            regex: /^[A-ZZŠĐĆČ]+(([',. -][a-zšđćčA-ZŠĐĆČ])?[a-zšđćčA-ZŠĐĆČ]*)*$/,
+            regex: /^[A-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/,
             errorMessage: "The last name is mandatory and must have the first initial capital letter."
         },
         {
@@ -246,45 +267,51 @@ function orderNow() {
     }
 
     if (!document.getElementById("agree-to-rules").checked) {
-        errorMessages.push("You must agree to the terms and conditions!")
+        errorMessages.push("You must agree to the terms and conditions!");
     }
 
     if (!document.querySelector('input[name="payment-method"]:checked')) {
-        errorMessages.push("Select a payment method!")
+        errorMessages.push("Select a payment method!");
     }
 
     if (errorMessages.length > 0) {
-        writeErrors(errorMessages)
+        writeErrors(errorMessages);
     } else {
-        writeSuccess()
+        writeSuccess();
     }
 }
 
+// Funkcija koja vrši validaciju unetih podataka u formi koristeći regularni izraz
 function validateField(fieldValue, regex) {
     return regex.test(fieldValue);
 }
 
+//Funkcija koja Generiše poruke o greškama i prikazuje ih korisniku
 function writeErrors(greske) {
-    populateModal("none", "Invalid information", greske.map(greska => `<p class="text-danger">${greska}</p>`).join(""))
+    populateModal("none", "Invalid information", greske.map(greska => `<p class="text-danger">${greska}</p>`).join(""));
 }
 
+// Funkcija koja generiše potvrdu o uspešnoj narudžbini i prikazuje je korisniku
 function writeSuccess() {
     let modalBody = "<h3>Are you sure you want to place the order?</h3>"
     modalBody += `<p>Name: ${document.getElementById("name").value}</p>`
     modalBody += `<p>Credit Card Type: ${document.querySelector('input[name="payment-method"]:checked').value}</p>`
-    populateModal("block", "Order Confirmation", modalBody)
+    populateModal("block", "Order Confirmation", modalBody);
 }
 
+// Funkcija koja popunjava modalni prozor sa određenim naslovom, sadržajem i opcijom za naručivanje
 function populateModal(orderButtonDisplay, modalTitle, modalBody) {
-    let modalBodyDiv = document.getElementById("modalBodyDiv")
-    let modalTitleH1 = document.getElementById("modalTitleH1")
-    let placeOrderButton = document.getElementById("placeOrderButton")
-    placeOrderButton.style.display = orderButtonDisplay
-    modalTitleH1.innerHTML = modalTitle
-    modalBodyDiv.innerHTML = modalBody
+    let modalBodyDiv = document.getElementById("modalBodyDiv");
+    let modalTitleH1 = document.getElementById("modalTitleH1");
+    let placeOrderButton = document.getElementById("placeOrderButton");
+    placeOrderButton.style.display = orderButtonDisplay;
+    modalTitleH1.innerHTML = modalTitle;
+    modalBodyDiv.innerHTML = modalBody;
 }
 
+// Funkcija koja obavlja procesiranje narudžbine, čisti korpu i preusmerava korisnika na početnu stranicu, sa dodatnim prikazom potvrde o uspešnoj narudžbini
 function order() {
-    localStorage.removeItem("cart")
-    window.location.href = "/"
+    localStorage.removeItem("cart");
+    localStorage.setItem('showSuccessfullOrderModal', 'true');
+    window.location.href = "/";
 }
